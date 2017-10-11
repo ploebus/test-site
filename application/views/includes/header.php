2a711590bfbb77577067fcf9492fd25359e715ec
Garvey <jgarvey@F5ECC.local>
2017-10-11 16:39:26 -0700

    datepicker

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -15,7 +15,10 @@
 	<?php echo link_tag('assets/stylesheets/datepicker.css'); ?>
 	
 	<script type="text/javascript" src="<?php echo base_url();?>assets/javascripts/bootstrap-datepicker.js"></script>
+    
     <?php echo link_tag('assets/stylesheets/style.css'); ?>
+
+
 <style type="text/css">
 	.navbar-inverse {
 		background-color: lightblue
