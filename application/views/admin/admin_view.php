aa2806ba0145bf7f8ced7290b940907992a00a5c
Garvey <jgarvey@F5ECC.local>
2018-04-02 13:35:02 -0700

    download data

--- application/views/admin/admin_view.php
+++ application/views/admin/admin_view.php
@@ -154,7 +154,22 @@
 			event.preventDefault();
 			console.log('Success')
 			$.post('download_file',function(data){
-				console.log(data)
+				  var items = data;
+        const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+        const header = Object.keys(items[0]);
+        let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+        csv.unshift(header.join(','));
+        csv = csv.join('\r\n');
+
+        //Download the file as CSV
+        var downloadLink = document.createElement("a");
+        var blob = new Blob(["\ufeff", csv]);
+        var url = URL.createObjectURL(blob);
+        downloadLink.href = url;
+        downloadLink.download = "DataDump.csv";  //Name the file here
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
 			});
 		});
 
