6a59e71f2fc203c928e15c982a9c7df8132fd867
Garvey <jgarvey@F5ECC.local>
2018-04-02 14:03:43 -0700

    download data

--- application/views/admin/admin_view.php
+++ application/views/admin/admin_view.php
@@ -10,7 +10,8 @@
 		<h5><a href="archived_children"><i class="glyphicon glyphicon-th-list"></i> Click here to see inactive or closed clients</a></h5>
 	</div>
 	<div class='col-md-5'>
-		<h5 id="downloadData"><a href="#" ><i class="glyphicon glyphicon-save"></i> Click here to download all records</a></h5>
+		<h5 id="downloadData"><a href="#" ><i class="glyphicon glyphicon-save"></i> Click here to download child file</a></h5>
+		<h5 id="downloadActivityData"><a href="#" ><i class="glyphicon glyphicon-save"></i> Click here to download activity file</a></h5>
 	</div>
 	
 	</div>
@@ -155,21 +156,21 @@
 			console.log('Success')
 			$.getJSON('download_file',function(data){
 				  var items = data;
-        const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
-        const header = Object.keys(items[0]);
-        let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
-        csv.unshift(header.join(','));
-        csv = csv.join('\r\n');
+        	const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+        	const header = Object.keys(items[0]);
+        	let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+        	csv.unshift(header.join(','));
+        	csv = csv.join('\r\n');
 
-        //Download the file as CSV
-        var downloadLink = document.createElement("a");
-        var blob = new Blob(["\ufeff", csv]);
-        var url = URL.createObjectURL(blob);
-        downloadLink.href = url;
-        downloadLink.download = "Child.csv";  //Name the file here
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
+        	//Download the file as CSV
+        	var downloadLink = document.createElement("a");
+        	var blob = new Blob(["\ufeff", csv]);
+        	var url = URL.createObjectURL(blob);
+        	downloadLink.href = url;
+        	downloadLink.download = "Child.csv";  //Name the file here
+        	document.body.appendChild(downloadLink);
+        	downloadLink.click();
+        	document.body.removeChild(downloadLink);
 			});
 		});
 
